2e98a85d6d5f838ec04d0c83f0de9acd197efbd1
Fady Khallaf <fady.khallaf@gmail.com>
2021-12-20 21:57:54 +0200

    add status endpoint

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, Response, json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/status')
+def status():
+    res = {'result': 'OK - healthy'}
+    response = app.response_class(response=json.dumps(res),
+     status=200, 
+     mimetype='application/json')
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
